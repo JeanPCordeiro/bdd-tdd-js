cabc9121c563d9fb37db5dd36c16f14f2e3b8b61
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2025-02-24 17:51:08 +0000

    more tdd

--- SimulPret/test/simulationPret.test.js
+++ SimulPret/test/simulationPret.test.js
@@ -8,3 +8,11 @@ test('devrait calculer les mensualités correctement', () => {
   expect(mensualites).toBeCloseTo(659.96, 2);
 });
 
+test('devrait calculer les mensualités correctement', () => {
+    const montant = 100000;
+    const taux = 0.07;
+    const duree = 20;
+    const mensualites = calculerMensualites(montant, taux, duree);
+    expect(mensualites).toBeCloseTo(775.30 , 2);
+  });
+
